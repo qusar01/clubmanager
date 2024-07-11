8b790e73270d5063984f081b2d062382b083d38a
qusar01 <oskar.baranowski@interia.pl>
2024-07-11 18:50:04 +0200

    init

---
+++ backend/clubmanager/src/main/java/pl/clubmanager/clubmanager/ClubmanagerApplication.java
@@ -0,0 +1,13 @@
+package pl.clubmanager.clubmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClubmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClubmanagerApplication.class, args);
+	}
+
+}
---
+++ backend/clubmanager/src/test/java/pl/clubmanager/clubmanager/ClubmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.clubmanager.clubmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClubmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
